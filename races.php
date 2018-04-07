82729c668ceb66c3640ea37b6740a6034cb6f57f
IvanForSecurity <Gh_new1@#4un>
2018-04-07 23:36:52 +0300

    game

--- races.php
+++ races.php
@@ -4,8 +4,12 @@
         <link rel="stylesheet" href="my_style.css">
         <title>Spaceship test</title>
     </head>
-    <body onload="startGame()">
+    <body onload="startGame()" >
+        <div>
         <script src='spaceshiptest.js'></script>
+        </div>
+        Speed: <input id='speed' type="number"  style="margin-top: 0.2em"><br>
+        Angles: <input id='angles' type="number"  style="margin-top: 0.2em"><br>
         <p>Make sure the gamearea has focus, and use the arrow keys to move the red square around.</p>
 
     </body>
--- spaceshiptest.js
+++ spaceshiptest.js
@@ -1,78 +1,117 @@
-var myGamePiece;
+var RedSquare;
+var backgroundstars = [];
+var WIDTH = 1000, HEIGHT = 500;
+var updateNum = 0;
+function startGame() {
 
-            function startGame() {
-                myGamePiece = new component(30, 30, "red", 225, 225);
-                myGameArea.start();
-            }
+    RedSquare = new component(60, 60, "red", WIDTH / 2, HEIGHT - 30);
+    myGameArea.start();
+}
 
-            var myGameArea = {
-                canvas: document.createElement("canvas"),
-                start: function () {
-                    this.canvas.width = 480;
-                    this.canvas.height = 270;
-                    this.context = this.canvas.getContext("2d");
-                    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-                    this.frameNo = 0;
-                    this.interval = setInterval(updateGameArea, 20);
-                    window.addEventListener('keydown', function (e) {
-                        e.preventDefault();
-                        myGameArea.keys = (myGameArea.keys || []);
-                        myGameArea.keys[e.keyCode] = (e.type == "keydown");
-                    })
-                    window.addEventListener('keyup', function (e) {
-                        myGameArea.keys[e.keyCode] = (e.type == "keydown");
-                    })
-                },
-                stop: function () {
-                    clearInterval(this.interval);
-                },
-                clear: function () {
-                    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-                }
-            }
+var myGameArea = {
+    canvas: document.createElement("canvas"),
+    start: function () {
+        this.canvas.width = WIDTH;
+        this.canvas.height = HEIGHT;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.keys = (myGameArea.keys || []);
+            myGameArea.keys[e.keyCode] = true;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.keys[e.keyCode] = false;
+        })
+    },
+    clear: function () {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-            function component(width, height, color, x, y, type) {
+    }
+}
 
-                this.type = type;
-                this.width = width;
-                this.height = height;
-                this.speed = 0;
-                this.angle = 0;
-                this.moveAngle = 0;
-                this.x = x;
-                this.y = y;
-                this.update = function () {
-                    ctx = myGameArea.context;
-                    ctx.save();
-                    ctx.translate(this.x, this.y);
-                    ctx.rotate(this.angle);
-                    ctx.fillStyle = color;
-                    ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
-                    ctx.restore();
-                }
-                this.newPos = function () {
-                    this.angle += this.moveAngle * Math.PI / 180;
-                    this.x += this.speed * Math.sin(this.angle);
-                    this.y -= this.speed * Math.cos(this.angle);
-                }
-            }
 
-            function updateGameArea() {
-                myGameArea.clear();
-                myGamePiece.moveAngle = 0;
-                myGamePiece.speed = 0;
-                if (myGameArea.keys && myGameArea.keys[37]) {
-                    myGamePiece.moveAngle = -1;
-                }
-                if (myGameArea.keys && myGameArea.keys[39]) {
-                    myGamePiece.moveAngle = 1;
-                }
-                if (myGameArea.keys && myGameArea.keys[38]) {
-                    myGamePiece.speed = 1;
-                }
-                if (myGameArea.keys && myGameArea.keys[40]) {
-                    myGamePiece.speed = -1;
-                }
-                myGamePiece.newPos();
-                myGamePiece.update();
-            }
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.angle = 0;
+    this.moveAngle = 0;
+    this.newPos = function () {
+        this.angle += this.moveAngle * Math.PI / 180;
+        this.x += this.speed * Math.sin(this.angle);
+        this.y -= this.speed * Math.cos(this.angle);
+    }
+    this.update = function () {
+        ctx = myGameArea.context;
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        var img = new Image();
+        if (updateNum < 20)
+        {
+            img.src = 'img/1.jpg';
+        }
+        if (updateNum < 40 && updateNum > 20)
+        {
+            img.src = 'img/2.jpg';
+        }
+        if (updateNum > 40) {
+            img.src = 'img/3.jpg';
+        }
+        ctx.drawImage(img, this.width / -2, this.height / -2, this.width, this.height)
+        ctx.restore();
+
+    }
+    this.updates = function (some) {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        this.y += some;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    updateNum++;
+    myGameArea.clear();
+    RedSquare.speed = 0;
+    RedSquare.speed = 0;
+    RedSquare.moveAngle = 0;
+    var speed_tmp =2;
+    var angle_tmp=1;
+    if (document.getElementById("speed").value) speed_tmp = document.getElementById("speed").value;
+    if (document.getElementById("angles").value) angle_tmp = document.getElementById("angles").value;
+    if (myGameArea.keys && myGameArea.keys  [37]) {
+        RedSquare.moveAngle = -1*(angle_tmp);
+    }
+    if (myGameArea.keys && myGameArea.keys  [39]) {
+        RedSquare.moveAngle = angle_tmp;
+    }
+    if (myGameArea.keys && myGameArea.keys  [38]) {
+        RedSquare.speed = speed_tmp;
+    }
+    if (myGameArea.keys && myGameArea.keys  [40]) {
+        RedSquare.speed = -1*(speed_tmp);
+    }
+    RedSquare.newPos();
+    RedSquare.update();
+    if (updateNum % 5 == 0)
+    {
+        if (updateNum == 60)
+            updateNum = 0;
+        backgroundstars.push(new component(3, 3, "white", gR(0, 1000), 0));
+
+    }
+    for (var i = 0; i < backgroundstars.length; i++)
+    {
+        backgroundstars[i].updates(1);
+    }
+
+}
+
+function gR(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
