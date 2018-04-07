6e7bef6229aec47894fa9435e9998d867fda2044
IvanForSecurity <Gh_new1@#4un>
2018-04-07 19:59:05 +0300

    1

---
+++ index2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="my_style.css">
+        <title>Authorisation</title>
+    </head>
+    <body>
+        <div style="align-content: center;text-align: center;margin-top: 10em">
+            <em>Authorisation</em><br><br>
+            <form action="races.php" method="get" >
+            <input type="text" name="login" placeholder="login" style="margin-top: 0.2em"><br>
+            <input type="password" name="pass" placeholder="password" style="margin-top: 0.2em"><br>
+            <input type="submit" name="LogInF" value="Log In" style="margin-top: 0.2em" ><br>
+            <input type="submit" name="RegF" value="Registration" style="margin-top: 0.2em"><br>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ races.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet" href="my_style.css">
+        <title>Spaceship test</title>
+    </head>
+    <body onload="startGame()">
+        <script src='spaceshiptest.js'></script>
+        <p>Make sure the gamearea has focus, and use the arrow keys to move the red square around.</p>
+
+    </body>
+</html>
