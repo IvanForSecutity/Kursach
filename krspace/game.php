6bf5a3b3909e6e8c397d827a577c632a85fbbbad
IvanForSecutity <v00900p@gmail.com>
2018-05-19 19:11:19 +0300

    [codedrop] rockets correct movement, code looks better, images replaces

--- krspace/game.php
+++ krspace/game.php
@@ -35,52 +35,48 @@ $speed = ((5000 + $engine_speed) * $free_capacity) / ($full_capacity * $full_cap
 ?>
 
 <html>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" href="style/my_style.css">
-        <title>Spaceship test</title>
-		
-    </head>
-    <body onload="startGame()" >
-        <div style="text-align: right; padding-right: 50px; padding-top: 10px;">
-            <a href="logout.php">Log Out</a>
-        </div>
-        <div id='gamediv'>
-            <script src='js/spaceshiptest.js'></script>
-        </div>
-        
-        Hp: <input id='hp' type="number" value="<?php echo $hp;?>" style="margin-top: 0.2em">
-        <br>
-        Speed: <input id='speed' type="number" value="<?php echo $speed;?>" style="margin-top: 0.2em">
-        <br>
-        Maneuverability: <input id='maneuverability' type="number" value="<?php echo $maneuverability;?>" style="margin-top: 0.2em">
-        <br>
-        Fuel tank volume: <input id='fuel_tank_volume' type="number" value="<?php echo $fuel_tank_volume;?>" style="margin-top: 0.2em">
-        <br>
-        
-        X: <input id='x' type="number" value="0" style="margin-top: 0.2em">
-        <br>
-        Y: <input id='y' type="number" value="0" style="margin-top: 0.2em">
-        <br>
 
-        TODO:
-        Это затычка, которую надо потом спрятать куда-нибудь...
-        <br>
-        Hull: <input id='ship_hull' type="text" value="<?php echo $hull['image'];?>" style="margin-top: 0.2em">
-        <br>
-        Engine: <input id='ship_engine' type="text" value="<?php echo $engine['image'];?>" style="margin-top: 0.2em">
-        <br>
-        Fuel tank: <input id='ship_fuel_tank' type="text" value="<?php echo $fuel_tank['image'];?>" style="margin-top: 0.2em">
-        <br>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style/my_style.css">
+  <title>Spaceship test</title>
 
-        <p>Make sure the gamearea has focus, and use the arrow keys to move the red square around.</p>
+</head>
+
+<body onload="startGame()">
+  <div style="text-align: right; padding-right: 50px; padding-top: 10px;">
+    <a href="logout.php">Log Out</a>
+  </div>
+  <div id='gamediv'>
+    <div id='game_field' >
+      <script src='js/spaceshiptest.js'></script>
+    </div>
+    <div style="border: 4px double black;">
+      <button id='stop_button' type="button">STOP</button>
+      <p id='helptext'>56</p>
+    </div>
+
+  </div>
         
+  Hp: <input id='hp' type="number" value="<?php echo $hp;?>" style="margin-top: 0.2em">
+  <br> Speed: <input id='speed' type="number" value="<?php echo $speed;?>" style="margin-top: 0.2em">
+  <br> Maneuverability: <input id='maneuverability' type="number" value="<?php echo $maneuverability;?>" style="margin-top: 0.2em">
+  <br> Fuel tank volume: <input id='fuel_tank_volume' type="number" value="<?php echo $fuel_tank_volume;?>" style="margin-top: 0.2em">
+  <br> TODO: Это затычка, которую надо потом спрятать куда-нибудь...
+  <br> Hull: <input id='ship_hull' type="text" value="<?php echo $hull['image'];?>" style="margin-top: 0.2em">
+  <br> Engine: <input id='ship_engine' type="text" value="<?php echo $engine['image'];?>" style="margin-top: 0.2em">
+  <br> Fuel tank: <input id='ship_fuel_tank' type="text" value="<?php echo $fuel_tank['image'];?>" style="margin-top: 0.2em">
+  <br>
+
+  <p>Make sure the gamearea has focus, and use the arrow keys to move the red square around.</p>
+
         TODO: Крутить разные мелодии в рандомном порядке.
-        <audio controls autoplay="true" loop="true" hidden="true">
+  <audio controls autoplay="true" loop="true" hidden="true">
             <source src="audio/Fly 2.ogg" type="audio/ogg; codecs=vorbis">
             <source src="audio/Fly 2.mp3" type="audio/mpeg">
             Тег audio не поддерживается вашим браузером.
             <a href="audio/Fly 2.mp3">Скачайте музыку</a>.
         </audio>
-    </body>
+</body>
+
 </html>
--- krspace/js/spaceshiptest.js
+++ krspace/js/spaceshiptest.js
@@ -1,328 +1,318 @@
-var myGamePiece;
+var spaceship;
 var myBackground;
-var FH = 500;
-var FW = 500;
-var speed_x;
-var speed_y;
-var obstacles_arr = [];
-var updateNum=0;
-
-var S_x=0;
-var S_y=0;
-var offset_x=0;
-var offset_y=0;
+var FH = 700;
+var FW = 1200;
 var PIC_H = 9000;
 var PIC_W = 3000;
-var ship_x =0;
-var ship_hull = "";
-var ship_y =0;
+var STOP = false;
+var obstacles_arr = [];
+var updateNum = 0;
+var all_drawn = false;
 
-function startGame()
-{
-    ship_hull = document.getElementById("ship_hull").value + "1.png";
+class Weapons {
+  constructor() {
+    this.units = [];
+    this.number = 0;
+  }
+  add_unit(a, b, c, d) {
+    this.units.push(new unit_w(a, b, c, d));
+    this.number++;
+  }
+  trydraw() {
 
+    if (this.number != 0) {
+      this.units.forEach(function(item, i, arr) {
+        item.move();
+      });
+    }
+  }
+}
+//weapon unit
+class unit_w {
+  constructor(f_x, f_y, t_x, t_y) {
+    this.x = f_x;
+    this.y = f_y;
+    this.to_x = t_x;
+    this.to_y = t_y;
+    this.speedX = (t_x-f_x)/100;
+    this.speedY = (t_y-f_y)/100;
+    this.img = new Image();
+    this.img.src = "images/Weapon units/rocket.png";
+    this.w = 45;
+    this.h = 90;
+    this.a = -30;
+  }
+  move() {
+    ctx = myGameArea.context;
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    var radians = Math.atan((this.speedY/this.speedX));
+    var degrees = radians* 180 / Math.PI+90;
+    if(this.speedX<0)degrees+=180;
+    document.getElementById("helptext").innerHTML = ""+degrees;
+    ctx.rotate(degrees* Math.PI/180);
+    ctx.drawImage(this.img, this.w / -2+ this.speedX, this.h / -2 +this.speedY, this.w, this.h)
+    this.x += this.speedX + myBackground.offset_x;
+    this.y += this.speedY + myBackground.offset_y;
+    ctx.restore();
+  }
+}
+var weapons = new Weapons();
 
-    
-    //create spaceship element
-    myGamePiece = new component(30, 50, ship_hull, FW/2, FH/2, "image");
-    myBackground = new component(PIC_W, PIC_H, "images/space.png", 0-PIC_W/2-FW/2, 0-PIC_H+FW, "image");
-    myGameArea.start();
-	var obstacles = {start: this.interval = setInterval(gen_obstacles, 200)};
-	
+function startGame() {
+  spaceship = new component(30, 50, "", FW / 2, FH*4 / 5);
+  myBackground = new component(PIC_W, PIC_H, "images/space.png", - PIC_W / 2 + FW / 2, 0 - PIC_H/2 + FH/2);
+  var obstacles = {
+    start: this.interval = setInterval(gen_obstacles, 1000)
+  };
+  //handle stop_button
+  var docStopBt = document.getElementById("stop_button");
+  docStopBt.onclick = function() {
+    if (docStopBt.innerHTML == "STOP") {
+      docStopBt.innerHTML = "START";
+      STOP = true;
+    } else if (docStopBt.innerHTML == "START") {
+      docStopBt.innerHTML = "STOP";
+      STOP = false;
+    }
+  };
+  //handle aim movement
+  myGameArea.canvas.addEventListener('mousemove', function(e) {
+    if (!STOP) {
+      ctx = myGameArea.context;
+      var aim = new Image();
+      aim.src = "images/aim_1.png";
+      var w = 90;
+      var h = 90;
+      ctx.drawImage(aim,
+        e.pageX - (w/2 + 8),
+        e.pageY - (h/2 + 8),
+        w, h);
+    }
+  }, false);
+  //handle gamefield click
+  myGameArea.canvas.addEventListener('mousedown', function(e) {
+    if (!STOP) {
+      //send rocket
+      var x_from = spaceship.x;
+      var x_to = e.pageX - (45 + 8);
+      var y_from = spaceship.y;
+      var y_to = e.pageY - (45 + 8);
+      weapons.add_unit(x_from, y_from, x_to, y_to);
+    }
+  }, false);
+  myGameArea.start();
+
 }
 var myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = FW;
-        this.canvas.height = FH;
-		this.canvas.addEventListener('mousemove', function(e) {
-			ctx = myGameArea.context;
-			var docElem = document.getElementById("gamediv");
-			var rect = docElem.getBoundingClientRect();
-
-			document.getElementById("x").value = e.clientY;
+  canvas: document.createElement("canvas"),
+  start: function() {
+    this.canvas.width = FW;
+    this.canvas.height = FH;
 			document.getElementById("y").value = e.pageY;
-			send_rocket(0,0,e.pageX,e.pageY);
-		}, false);
-		
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.frameNo = 0;
-        this.interval = setInterval(updateGameArea, 20);
-        window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = true;
-        })
-        window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = false;
-        })
-        
-    },
-    clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    stop: function () {
-        clearInterval(this.interval);
-    }
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);
+    window.addEventListener('keydown', function(e) {
+      myGameArea.keys = (myGameArea.keys || []);
+      myGameArea.keys[e.keyCode] = true;
+    })
+    window.addEventListener('keyup', function(e) {
+      myGameArea.keys[e.keyCode] = false;
+    })
+
+  },
+  clear: function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  },
+  stop: function() {
+    clearInterval(this.interval);
+  }
 }
-var rockets=0;
-function send_rocket(from_x,from_y,to_x,to_y)
-{
-	
-	//rockets++;
-	
-	//if(rockets==100)
-	{
-		
-		ctx = myGameArea.context;
-		var sticky = new Image();
-		sticky.src = "images/aim_1.png";
-		ctx.drawImage(sticky,
-					to_x,
-					to_y,
-					90, 90);	
-	}
+
+
+function component(width, height, img, x, y) {
+  this.image = new Image();
+  this.image.src = img;
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  //vars [backgound] for correct meteors movement
+  this.offset_x = 0;
+  this.offset_y = 0;
+  //ship
+  this.ship_hull = document.getElementById("ship_hull").value + "1.png";
+  this.angle = 0;
+  this.A = 0;
+  this.moveAngle = 0;
+  this.update = function() {
+    ctx = myGameArea.context;
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+
+    ctx.drawImage(this.image, this.width / -2, this.height / -2, this.width, this.height)
+    ctx.restore();
+  }
+  this.updateb = function() {
+    ctx = myGameArea.context;
+    ctx.drawImage(this.image,
+      this.x,
+      this.y,
+      this.width, this.height);
+  }
+  this.newPos = function() {
+    this.angle += this.moveAngle * Math.PI / 180;
+    this.A += this.moveAngle;
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  this.newPosb = function() {
+    this.offset_x = this.speedX;
+    //this.offset_x += this.speedX;
+    this.offset_y = this.speedY;
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  this.updateAnimation = function() {
+    this.image.src = this.ship_hull;
+  }
 }
-function component(width, height, img, x, y, type) {
-    this.type = type;
-    if (type == "image") {
-        this.image = new Image();
-        this.image.src = img;
+
+function obstacle(w, h, img, x, y) {
+  this.image = new Image();
+  this.image.src = img;
+  this.width = w;
+  this.height = h;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.init = x;
+  this.y = y;
+  this.updates = function() {
+    ctx = myGameArea.context;
+    ctx.drawImage(this.image,
+      this.x,
+      this.y,
+      this.width, this.height);
+  }
+
+  this.newPosb = function() {
+    this.x += myBackground.offset_x + myBackground.offset_x;
+    this.y += 5 + myBackground.offset_y;
+
+  }
+  this.crashWith = function(otherobj) {
+    var myleft = this.x;
+    var myright = this.x + (this.width);
+    var mytop = this.y;
+    var mybottom = this.y + (this.height);
+    var otherleft = otherobj.x;
+    var otherright = otherobj.x + (otherobj.width);
+    var othertop = otherobj.y;
+    var otherbottom = otherobj.y + (otherobj.height);
+    var crash = true;
+    if ((mybottom < othertop) ||
+      (mytop > otherbottom) ||
+      (myright < otherleft) ||
+      (myleft > otherright)) {
+      crash = false;
     }
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.angle = 0;
-    this.A = 0;
-    this.moveAngle = 0;
-    this.update = function () {
-        ctx = myGameArea.context;
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(this.angle);
-        
-        ctx.drawImage(this.image, this.width / -2, this.height / -2, this.width, this.height)
-        ctx.restore();
-    }
-	
-    this.updateb = function () {
-        ctx = myGameArea.context;
-        ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
-    }
-    this.newPos = function () {
-        this.angle += this.moveAngle * Math.PI / 180;
-		this.A += this.moveAngle ;
-        this.x += this.speedX;
-        this.y += this.speedY;
-		ship_x = this.x;
-		ship_y = this.y;
-    }
-    this.newPosb = function () {
-		//document.getElementById("speed").value = offset_x;
-		offset_x += this.speedX;
-		offset_y = this.speedY;
-        this.x += this.speedX;
-        this.y += this.speedY;
-        this.y +=0.3;
-		S_y = this.y;
-		S_x = this.x;
-    }
-    this.updateAnimation = function()
-    {
-        this.image.src = ship_hull;
-    }
-}
-function obstacle(w,h,img, x, y) {
-    this.image = new Image();
-    this.image.src = img;
-    this.width = w;
-    this.height = h;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-	this.init = x;
-    this.y = y;
-	this.updates = function () {
-        ctx = myGameArea.context;
-		ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);	
-    }
-	
-    this.newPosb = function () {
-        this.x = this.init+offset_x;
-        this.y += 5+offset_y;
-		
-    }
-	this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) ||
-               (mytop > otherbottom) ||
-               (myright < otherleft) ||
-               (myleft > otherright)) {
-           crash = false;
-        }
-        return crash;
-    }
+    return crash;
+  }
 }
 //every 20 miliseconds
-function updateGameArea()
-{
+function updateGameArea() {
+  if (!STOP) {
     updateNum++;
-    if (updateNum == 100)
-    {
-        updateNum = 0;
+    if (updateNum == 100) {
+      updateNum = 0;
     }
-    
+
     // Animation phases
-    if (updateNum < 20)
-    {
-        ship_hull = document.getElementById("ship_hull").value + "1.png";
+    if (updateNum < 20) {
+      spaceship.ship_hull = document.getElementById("ship_hull").value + "1.png";
     }
-    if (updateNum >= 20 && updateNum < 40)
-    {
-        ship_hull = document.getElementById("ship_hull").value + "2.png";
+    if (updateNum >= 20 && updateNum < 40) {
+      spaceship.ship_hull = document.getElementById("ship_hull").value + "2.png";
     }
-    if (updateNum >= 40 && updateNum < 60)
-    {
-        ship_hull = document.getElementById("ship_hull").value + "3.png";
+    if (updateNum >= 40 && updateNum < 60) {
+      spaceship.ship_hull = document.getElementById("ship_hull").value + "3.png";
     }
-    if (updateNum >= 60 && updateNum < 80)
-    {
-        ship_hull = document.getElementById("ship_hull").value + "4.png";
+    if (updateNum >= 60 && updateNum < 80) {
+      spaceship.ship_hull = document.getElementById("ship_hull").value + "4.png";
     }
-    if (updateNum >= 80 && updateNum < 100)
-    {
-        ship_hull = document.getElementById("ship_hull").value + "5.png";
+    if (updateNum >= 80 && updateNum < 100) {
+      spaceship.ship_hull = document.getElementById("ship_hull").value + "5.png";
     }
-    
+
     myGameArea.clear();
-    myBackground.speedY = 0; 
-    myBackground.speedX = 0; 
-    myGamePiece.moveAngle = 0;
-    var maneuverability_tmp = document.getElementById("maneuverability").value; 
+    myBackground.speedY = 0;
+    myBackground.speedX = 0;
+    spaceship.moveAngle = 0;
+    var maneuverability_tmp = document.getElementById("maneuverability").value;
     var speed_tmp = document.getElementById("speed").value;
-	if(myGameArea.keys)
-	{
-		var L = myGameArea.keys  [37];
-		var R = myGameArea.keys  [39];
-		var F = myGameArea.keys  [38];
-		var B = myGameArea.keys  [40];
-		
-		if(L||F||R||B)
-		{
-			
-			var cos_angle = Math.cos(myGamePiece.angle);
-			var sin_angle = Math.sin(myGamePiece.angle);
-			if(L)	myGamePiece.moveAngle = -1*(maneuverability_tmp);
-			if(R)	myGamePiece.moveAngle = maneuverability_tmp;
-			if(F||B)
-			{
-				var dir=1;
-				if(B)dir=-1;
-				myBackground.speedX = -1*	dir*speed_tmp*sin_angle;
-				myBackground.speedY = 		dir*speed_tmp*cos_angle;
-			}
-			
-		}
-		
-	}
-	
+    if (myGameArea.keys) {
+      var L = myGameArea.keys[37];
+      var R = myGameArea.keys[39];
+      var F = myGameArea.keys[38];
+      var B = myGameArea.keys[40];
+
+      if (L || F || R || B) {
+
+        var cos_angle = Math.cos(spaceship.angle);
+        var sin_angle = Math.sin(spaceship.angle);
+        if (L) spaceship.moveAngle = -1 * (maneuverability_tmp);
+        if (R) spaceship.moveAngle = maneuverability_tmp;
+        if (F || B) {
+          var dir = 1;
+          if (B) dir = -1;
+          myBackground.speedX = -1 * dir * speed_tmp * sin_angle;
+          myBackground.speedY = dir * speed_tmp * cos_angle;
+        }
+
+      }
+
+    }
+
     myBackground.newPosb();
     myBackground.updateb();
-    myGamePiece.newPos();
-    myGamePiece.update();
-    myGamePiece.updateAnimation();
-	for (var i = 0; i < obstacles_arr.length; i++)
-	{
-		if(obstacles_arr[i].crashWith(myGamePiece))
-		{
-			
-			myGameArea.stop();
-		}
-		obstacles_arr[i].updates();
-		obstacles_arr[i].newPosb();
-	}
-}
-
-function gen_obstacles()
-{
-	var o_num = obstacles_arr.length%3;
-	switch(o_num)
-	{
-		case 0:
-			obstacles_arr.push(new obstacle(80,80,"images/meteor_1.png", S_x + gR(0,PIC_W), S_y + gR(0,1000)));
-			break;
-		case 1:
-			obstacles_arr.push(new obstacle(100,100,"images/meteor_2.png", S_x + gR(0,PIC_W), S_y + gR(0,1000)));
-			break;
-		case 2:
-			obstacles_arr.push(new obstacle(150,150,"images/meteor_3.png", S_x + gR(0,PIC_W), S_y + gR(0,1000)));
-			break;
-		
-	}
-	
-	
-}
-function gR(min, max)
-{
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-function getOffset(elem) {
-    if (elem.getBoundingClientRect) {
-        // "правильный" вариант
-        return getOffsetRect(elem)
-    } else {
-        // пусть работает хоть как-то
-        return getOffsetSum(elem)
+    //weapon unit is needed to be drawn before spaceship to be under it in canvas
+    weapons.trydraw();
+    spaceship.newPos();
+    spaceship.update();
+    spaceship.updateAnimation();
+    for (var i = 0; i < obstacles_arr.length; i++) {
+      if (obstacles_arr[i].crashWith(spaceship)) {
+        STOP = true;
+      }
+      obstacles_arr[i].updates();
+      obstacles_arr[i].newPosb();
     }
+  }
 }
 
-function getOffsetSum(elem) {
-    var top=0, left=0
-    while(elem) {
-        top = top + parseInt(elem.offsetTop)
-        left = left + parseInt(elem.offsetLeft)
-        elem = elem.offsetParent
+function gen_obstacles() {
+  if (!STOP) {
+    var o_num = obstacles_arr.length % 3;
+    switch (o_num) {
+      case 0:
+        obstacles_arr.push(new obstacle(80, 80, "images/Obstacles/meteor_1.png", myBackground.x + gR(0, PIC_W), myBackground.y + gR(0, 1000)));
+        break;
+      case 1:
+        obstacles_arr.push(new obstacle(100, 100, "images/Obstacles/meteor_2.png", myBackground.x + gR(0, PIC_W), myBackground.y + gR(0, 1000)));
+        break;
+      case 2:
+        obstacles_arr.push(new obstacle(150, 150, "images/Obstacles/meteor_3.png", myBackground.x + gR(0, PIC_W), myBackground.y + gR(0, 1000)));
+        break;
     }
-
-    return {top: top, left: left}
+  }
 }
 
-function getOffsetRect(elem) {
-    // (1)
-    var box = elem.getBoundingClientRect()
-
-    // (2)
-    var body = document.body
-    var docElem = document.documentElement
-
-    // (3)
-    var scrollTop = window.pageYOffset || docElem.scrollTop || body.scrollTop
-    var scrollLeft = window.pageXOffset || docElem.scrollLeft || body.scrollLeft
-
-    // (4)
-    var clientTop = docElem.clientTop || body.clientTop || 0
-    var clientLeft = docElem.clientLeft || body.clientLeft || 0
-
-    // (5)
-    var top  = box.top +  scrollTop - clientTop
-    var left = box.left + scrollLeft - clientLeft
-
-    return { top: Math.round(top), left: Math.round(left) }
+function gR(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
