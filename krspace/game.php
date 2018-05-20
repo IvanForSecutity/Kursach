e7ec0b6b6774abd24976c664614fbffab75e3419
IvanForSecutity <v00900p@gmail.com>
2018-05-20 14:07:20 +0300

    [fix] game field true fix

--- krspace/game.php
+++ krspace/game.php
@@ -18,11 +18,14 @@ $ship = loadCurrentShip($ship_name);
 $hull = loadShipHull($ship['hull']);
 $engine = loadShipEngine($ship['engine']);
 $fuel_tank = loadShipFuelTank($ship['fuel_tank']);
+$secondary_engines = loadShipSecondaryEngines($ship['secondary_engines']);
+$radar = loadShipRadar($ship['radar']);
+$repair_droid = loadShipRepairDroid($ship['repair_droid']);
 
 // Calculate parameters
 $hp = $hull['hp'];
 $full_capacity = $hull['capacity'];
-$maneuverability = $hull['maneuverability'];
+$maneuverability = $hull['maneuverability'] + $secondary_engines['maneuverability'];
 
 $engine_weight = $engine['weight'];
 $engine_speed = $engine['speed'];
@@ -30,8 +33,17 @@ $engine_speed = $engine['speed'];
 $fuel_tank_weight = $fuel_tank['weight'];
 $fuel_tank_volume = $fuel_tank['volume'];
 
-$free_capacity = $full_capacity - $engine_weight - $fuel_tank_weight;
+$secondary_engines_weight = $secondary_engines['weight'];
+
+$radar_weight = $radar['weight'];
+$radar_action_radius = $radar['action_radius'];
+
+$repair_droid_weight = $repair_droid['weight'];
+$repair_droid_health_recovery = $repair_droid['health_recovery'];
+
+$free_capacity = $full_capacity - $engine_weight - $fuel_tank_weight - $secondary_engines_weight - $radar_weight - $repair_droid_weight;
 $speed = ((5000 + $engine_speed) * $free_capacity) / ($full_capacity * $full_capacity);
+$cost = $hull['cost'] + $engine['cost'] + $fuel_tank['cost'] + $secondary_engines['cost'] + $radar['cost'] + $repair_droid['cost'];
 ?>
 
   <html>
@@ -54,7 +66,7 @@ $speed = ((5000 + $engine_speed) * $free_capacity) / ($full_capacity * $full_cap
       </div>
       <div style="border: 4px double black;display:inline">
         <button id='stop_button' type="button">STOP</button>
-        <p id='helptext'>rocket</p>
+        <a id='helptext'>helptext</a>
         Weapon type: <select id="WeaponType" class="select-multi" size="1" onchange="this.form.submit()">
           <option value="rocket"     > Rocket    </option>
           <option value="blaster"    > Blaster   </option>
@@ -74,10 +86,15 @@ $speed = ((5000 + $engine_speed) * $free_capacity) / ($full_capacity * $full_cap
         <br> Speed: <input id='speed' type="number" value="<?php echo $speed;?>" style="margin-top: 0.2em">
         <br> Maneuverability: <input id='maneuverability' type="number" value="<?php echo $maneuverability;?>" style="margin-top: 0.2em">
         <br> Fuel tank volume: <input id='fuel_tank_volume' type="number" value="<?php echo $fuel_tank_volume;?>" style="margin-top: 0.2em">
-        <br> TODO: Это затычка, которую надо потом спрятать куда-нибудь...
+       <br> Radar action radius: <input id='radar_action_radius' type="number" value="<?php echo $radar_action_radius;?>" style="margin-top: 0.2em">
+       <br> Health recovery: <input id='hp' type="number" value="<?php echo $repair_droid_health_recovery;?>" style="margin-top: 0.2em">
+       <br> TODO: Это затычка, которую надо потом спрятать куда-нибудь...
         <br> Hull: <input id='ship_hull' type="text" value="<?php echo $hull['image'];?>" style="margin-top: 0.2em">
         <br> Engine: <input id='ship_engine' type="text" value="<?php echo $engine['image'];?>" style="margin-top: 0.2em">
         <br> Fuel tank: <input id='ship_fuel_tank' type="text" value="<?php echo $fuel_tank['image'];?>" style="margin-top: 0.2em">
+        <br> Secondary engines: <input id='ship_secondary_engines' type="text" value="<?php echo $secondary_engines['image'];?>" style="margin-top: 0.2em">
+        <br> Radar: <input id='ship_radar' type="text" value="<?php echo $radar['image'];?>" style="margin-top: 0.2em">
+        <br> Repair droid: <input id='ship_repair_droid' type="text" value="<?php echo $repair_droid['image'];?>" style="margin-top: 0.2em">
         <br>
 
       </div>
